20c224381e890672813c38d0adfa917cb7ab1524
Julien Breton <julienbreton@hotmail.fr>
2019-06-08 19:22:31 +0200

    test: use curl to upload a file

---
+++ src/main/java/curl/DoCurl.java
@@ -0,0 +1,21 @@
+package curl;
+
+import static org.toilelibre.libe.curl.Curl.curl;
+
+import org.apache.http.HttpResponse;
+
+public class DoCurl {
+
+	public String getGoogleHomepage() {
+		// -L is passed to follow the redirects
+		return curl().lUpperCase().$("https://www.google.com/");
+	}
+	
+	public HttpResponse uploadFile() {
+		
+		return curl("-X POST -F 'fileToUpload=@/home/julien/test/test.txt' http://localhost/upload.php");
+		
+		//return curl().k().xUpperCase("POST").fUpperCase("@/home/julien/test/test.txt").run("http://localhost/upload.php");
+	}
+
+}
---
+++ src/test/java/curl/TestCurl.java
@@ -0,0 +1,25 @@
+package curl;
+
+import org.apache.http.HttpResponse;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCurl {
+	
+	private DoCurl curl = new DoCurl();
+	
+	@Test
+	public void curlGoogle() {
+		
+		String sResult = curl.getGoogleHomepage();	
+		Assert.assertTrue(sResult.contains("Google"));
+	}
+	
+	@Test
+	public void curlUploadFile() {
+		
+		HttpResponse Response = curl.uploadFile();
+		System.out.print(Response.toString());
+	}
+
+}
